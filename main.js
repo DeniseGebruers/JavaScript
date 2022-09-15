9f17351be1f20a76a5dacc46c9744636806640c9
denise_gebruers <denise.gebruers@gmail.com>
2022-09-15 17:24:12 -0300

    1era entrega proyecto final

--- main.js
+++ main.js
@@ -2,65 +2,105 @@ class Producto {
     constructor (nombre, precio) {
         this.nombre = nombre;
         this.precio = parseFloat(precio);
-        this.cantidad = 8;
-        this.vendido = false;
+        this.cantidad = 2;
     }
 
     sumarIva () {
         return (this.precio * 1.21).toFixed(2);
     }
-
-    vender () {
-        this.vendido = true;
-    }
 }
-
-let nombre = prompt("Ingrese su nombre: ");
-console.log (nombre);
-let saludo = prompt ("Hola " + nombre + ", ingrese el producto que desea (producto1 o producto2)");
-console.log (saludo);
 
 const mateDiseño1 = new Producto ("Diseño1", 720);
 const mateDiseño2 = new Producto ("Diseño2", 850);
+const mateDiseño3 = new Producto ("Diseño3", 920);
 const accesorioMate = new Producto ("Bombilla", 325);
 
-mateDiseño1.vender();
-accesorioMate.vender();
+const stockProducto = [];
+
+stockProducto.push (mateDiseño1);
+stockProducto.push (mateDiseño2);
+stockProducto.push (mateDiseño3);
+stockProducto.push (accesorioMate);
+
+console.log (stockProducto);
 
 
-if (saludo === "producto1") {
-    alert ("Producto añadido. El precio es: $" + mateDiseño1.precio + ", con el IVA el precio final es: $" + mateDiseño1.sumarIva());
-} else if (saludo === "producto2") {
-    alert ("Producto añadido. El precio es: $" + mateDiseño2.precio + ", con el IVA el precio final es: $" + mateDiseño2.sumarIva());
-} else {
-    alert ("No es un producto existente. Vuelva a intentar.");
+/* Funciones */
+
+function menu () {
+    alert ("¡Bienvenido a nuestra web!");
+    let nombre = prompt ("Ingrese su nombre: ");
+    console.log (nombre);
+    let opcion = parseInt (prompt ("Hola " + nombre + ", ingrese el producto que desea: \n 1) Diseño1 \n 2) Diseño2 \n 3) Diseño3 \n 4) Salir"));
+    return opcion;
 }
 
-let accesorio = prompt ("Ingrese Bombilla para agregar el producto al carrito, si no lo desea ingrese SALIR");
+function producto1 () {
+    alert ("Producto añadido. Su pecio es: $" + mateDiseño1.precio + ", con el IVA el precio final es: $" + mateDiseño1.sumarIva());
+    let pregunta = prompt ("¿Desea añadir bombilla a su producto? Ingrese si o no");
+    while (pregunta === "si") {
+        alert ("El producto se ha añadido correctamente. Su precio es: $" + accesorioMate.precio + ", con el IVA el total es: $" + accesorioMate.sumarIva() + ". \n¡Muchas gracias por su compra!");
+        break;
+    } if (pregunta === "no") {
+        alert ("¡Muchas gracias por su compra!");
+    }  
+    let busqueda = stockProducto.find (Producto => Producto.nombre === "Diseño1");
+    console.log (busqueda);
+    let nuevoStock = stockProducto.splice (0,1);
+    console.log (nuevoStock);
+}
 
-const stockProducto = [mateDiseño1, mateDiseño2, accesorioMate];
+function producto2 () {
+    alert ("Producto añadido. Su pecio es: $" + mateDiseño2.precio + ", con el IVA el precio final es: $" + mateDiseño2.sumarIva());
+    let pregunta = prompt ("¿Desea añadir bombilla a su producto? Ingrese si o no");
+    while (pregunta === "si") {
+        alert ("El producto se ha añadido correctamente. Su precio es: $" + accesorioMate.precio + ", con el IVA el total es: $" + accesorioMate.sumarIva());
+        break;
+    } if (pregunta === "no") {
+        alert ("¡Muchas gracias por su compra!")
+    } 
+    let busqueda = stockProducto.find (Producto => Producto.nombre === "Diseño2");
+    console.log (busqueda);
+    let nuevoStock = stockProducto.splice (1,1);
+    console.log (nuevoStock);
+}
 
-function Bombilla (stockProducto) {
-    for (const Producto of stockProducto) {
-        if (Producto.nombre === "Bombilla" && accesorio === "Bombilla") {
-            alert("La Bombilla cuesta: $" + accesorioMate.precio + ", con el IVA el precio final es: $" + accesorioMate.sumarIva());
-            alert ("¡Gracias por tu compra " + nombre +  ", regresa pronto!");
-            } if (accesorio === "SALIR") {
-                alert ("Muchas gracias por su compra.");
-            }
-        }
+function producto3 () {
+    alert ("Producto añadido. Su pecio es: $" + mateDiseño3.precio + ", con el IVA el precio final es: $" + mateDiseño3.sumarIva());
+    let pregunta = prompt ("¿Desea añadir bombilla a su producto? Ingrese si o no");
+    while (pregunta === "si") {
+        alert ("El producto se ha añadido correctamente. Su precio es: $" + accesorioMate.precio + ", con el IVA el total es: $" + accesorioMate.sumarIva());
+        break;
+    } if (pregunta === "no") {
+        alert ("¡Muchas gracias por su compra!")
+    } 
+    let busqueda = stockProducto.find (Producto => Producto.nombre === "Diseño3");
+    console.log (busqueda);
+    let nuevoStock = stockProducto.splice (2,1);
+    console.log (nuevoStock);
+}
+
+function salir () {
+    alert ("¡Muchas gracias por visitarnos!");
+}
+
+/* Ejecución */
+
+let opcion = menu ();
+    switch (opcion) {
+        case 1:
+            producto1 ();
+            break;
+        case 2:
+            producto2 ();
+            break;
+        case 3:
+            producto3 ();
+            break;
+        case 4:
+            salir ();
+            break;
+        default:
+            alert ("Opción no válida.")
+            break;
     }
-
-Bombilla (stockProducto);
-console.log (stockProducto)
-
-
-
-let carrito =  stockProducto.slice (0,2);
-console.log (carrito);
-
-let indiceBombilla = stockProducto.indexOf(accesorioMate);
-console.log (indiceBombilla);
-
-let booleanoBombilla = stockProducto.includes (accesorioMate);
-console.log (booleanoBombilla);
